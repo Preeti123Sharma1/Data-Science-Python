1b8059acbb3b5bfd7b73facf23a481244487b151
Preeti123Sharma1 <108971201+Preeti123Sharma1@users.noreply.github.com>
2022-07-30 19:32:12 +0530

    Create first_letter1.py

---
+++ first_letter1.py
@@ -0,0 +1,18 @@
+from turtle import*
+
+pensize(5)
+pencolor('red')
+penup()
+circle( 100,180)
+pendown()
+circle(100,180)
+
+goto(0,60)
+
+goto(60,60)
+
+goto(60,-30)
+
+mainloop()
+
+## this is graph of G 
